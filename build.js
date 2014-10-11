a061c213b002765f2ba04303e60ff96a3d3ec379
Gavin Kistner <gavin@phrogz.net>
2014-10-10 22:19:35 -0600

    Ignore node_modules for Git

--- build.js
+++
@@ -1,9 +0,0 @@
-var peg  = require('pegjs')
-var path = require('path')
-var fs   = require('fs')
-
-var parser = peg.buildParser(
-  fs.readFileSync(path.resolve(__dirname, 'grammar.peg'), 'utf8')
-)
-
-console.log('module.exports = ' + parser.toSource())
