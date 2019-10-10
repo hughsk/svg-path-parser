a19ca4b09f11e05461472c45ab5e304a4148362e
Justin Barca <justin.barca@points.com>
2019-10-10 18:15:21 -0400

    add function to re-compose d attribute from a list of commands

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // v1.0 exported just the parser function. To maintain backwards compatibility,
 // we export additional named features as properties of that function.
 var parserFunction = require('./parser.js').parse;
+parserFunction.composeSVG = composeSVG;
 parserFunction.parseSVG = parserFunction;
 parserFunction.makeAbsolute = makeSVGPathCommandsAbsolute;
+parserFunction.mapCommandToString = mapCommandToString;
+
 module.exports = parserFunction;
 
 function makeSVGPathCommandsAbsolute(commands) {
@@ -23,4 +26,35 @@ function makeSVGPathCommandsAbsolute(commands) {
 		prevCmd = cmd;
 	});
 	return commands;
+}
+
+function mapCommandToString (cmd) {
+	var boolToInt = function (bool) {
+		return bool ? 1 : 0;
+	};
+	var arrayToCommandString = function (array) {
+		return array.map(function(coords) { return coords.join(',')}).join(' ');
+	};
+	if (cmd.code.toLowerCase() === 'a') {
+		return cmd.code +
+			arrayToCommandString([[cmd.rx, cmd.ry], [cmd.xAxisRotation], [boolToInt(cmd.largeArc), boolToInt(cmd.sweep)], [cmd.x, cmd.y]]);
+	}
+	var axisCommandIndex = ['v', 'h'].indexOf(cmd.code.toLowerCase());
+	if (axisCommandIndex !== -1) {
+		var lengthAttr = axisCommandIndex === 0 ? 'y' : 'x';
+		return cmd.code + cmd[lengthAttr];
+	}
+
+	if (cmd.code === 'Z') { return cmd.code; }
+
+	var intermediaryPointsIndices = Object.keys(cmd)
+		.filter(function(key){ return key.indexOf('x') === 0 && key.length === 2})
+		.map(function(key){ return key.replace(/\D/g,'')}).sort();
+
+	return cmd.code + arrayToCommandString(intermediaryPointsIndices.concat([''])
+		.map(function (strIndex) { return [cmd['x' + strIndex], cmd['y' + strIndex]]; }));
+}
+
+function composeSVG (commandList) {
+	return commandList.map(mapCommandToString).join(' ');
 }
