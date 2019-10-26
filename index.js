d8b2f92f3167569465653d48e4def3e93209f9eb
Justin Barca <justin.barca@points.com>
2019-10-26 14:08:31 -0400

    update docs, remove makeAbsolute

--- index.js
+++ index.js
@@ -4,30 +4,9 @@ var parserFunction = require('./parser.js').parse;
 parserFunction.composeSVG = composeSVG;
 parserFunction.parseSVG = parserFunction;
 
-parserFunction.makeAbsolute = makeSVGPathCommandsAbsolute;
 parserFunction.mapCommandToString = mapCommandToString;
 
 module.exports = parserFunction;
-
-function makeSVGPathCommandsAbsolute(commands) {
-	var subpathStart, prevCmd={x:0,y:0};
-	var attr = {x:'x0',y:'y0',x1:'x0',y1:'y0',x2:'x0',y2:'y0'};
-	commands.forEach(function(cmd) {
-		if (cmd.command==='moveto') subpathStart=cmd;
-		cmd.x0=prevCmd.x; cmd.y0=prevCmd.y;
-		for (var a in attr) if (a in cmd) cmd[a] += cmd.relative ? cmd[attr[a]] : 0;
-		if (!('x' in cmd)) cmd.x = prevCmd.x; // V
-		if (!('y' in cmd)) cmd.y = prevCmd.y; // X
-		cmd.relative = false;
-		cmd.code = cmd.code.toUpperCase();
-		if (cmd.command=='closepath') {
-			cmd.x = subpathStart.x;
-			cmd.y = subpathStart.y;
-		}
-		prevCmd = cmd;
-	});
-	return commands;
-}
 
 function mapCommandToString (cmd) {
 	var boolToInt = function (bool) {
